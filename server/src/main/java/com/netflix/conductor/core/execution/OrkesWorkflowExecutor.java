64edea10548931f01aa49f2a9bbe7ab864256480
Viren Baraiya <virenx@gmail.com>
2023-09-16 14:25:59 -0400

    security patches and upgrades (#33)

--- server/src/main/java/com/netflix/conductor/core/execution/OrkesWorkflowExecutor.java
+++ server/src/main/java/com/netflix/conductor/core/execution/OrkesWorkflowExecutor.java
@@ -16,10 +16,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Lazy;
@@ -29,11 +26,7 @@ import org.springframework.stereotype.Component;
 import com.netflix.conductor.common.metadata.workflow.RerunWorkflowRequest;
 import com.netflix.conductor.core.config.ConductorProperties;
 import com.netflix.conductor.core.dal.ExecutionDAOFacade;
-import com.netflix.conductor.core.exception.NonTransientException;
-import com.netflix.conductor.core.exception.NotFoundException;
-import com.netflix.conductor.core.exception.TerminateWorkflowException;
 import com.netflix.conductor.core.execution.tasks.SystemTaskRegistry;
-import com.netflix.conductor.core.execution.tasks.WorkflowSystemTask;
 import com.netflix.conductor.core.listener.TaskStatusListener;
 import com.netflix.conductor.core.listener.WorkflowStatusListener;
 import com.netflix.conductor.core.metadata.MetadataMapperService;
@@ -42,7 +35,6 @@ import com.netflix.conductor.core.utils.ParametersUtils;
 import com.netflix.conductor.core.utils.QueueUtils;
 import com.netflix.conductor.dao.MetadataDAO;
 import com.netflix.conductor.dao.QueueDAO;
-import com.netflix.conductor.metrics.Monitors;
 import com.netflix.conductor.model.TaskModel;
 import com.netflix.conductor.model.WorkflowModel;
 import com.netflix.conductor.redis.dao.RedisExecutionDAO;
@@ -50,7 +42,6 @@ import com.netflix.conductor.service.ExecutionLockService;
 
 import io.orkes.conductor.id.TimeBasedUUIDGenerator;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.netflix.conductor.core.utils.Utils.DECIDER_QUEUE;
@@ -66,7 +57,6 @@ public class OrkesWorkflowExecutor extends WorkflowExecutor {
 
     private final ExecutionDAOFacade orkesExecutionDAOFacade;
     private final SystemTaskRegistry systemTaskRegistry;
-    private final ExecutorService taskUpdateExecutor;
     private final RedisExecutionDAO executionDAO;
 
     public OrkesWorkflowExecutor(
@@ -103,26 +93,6 @@ public class OrkesWorkflowExecutor extends WorkflowExecutor {
         this.orkesExecutionDAOFacade = executionDAOFacade;
         this.systemTaskRegistry = systemTaskRegistry;
         this.executionDAO = executionDAO;
-
-        int threadPoolSize = Runtime.getRuntime().availableProcessors() * 10;
-        this.taskUpdateExecutor =
-                new ThreadPoolExecutor(
-                        threadPoolSize,
-                        threadPoolSize,
-                        0,
-                        TimeUnit.SECONDS,
-                        new ArrayBlockingQueue<>(threadPoolSize) {
-                            @Override
-                            public boolean offer(Runnable runnable) {
-                                try {
-                                    return super.offer(runnable, 100, TimeUnit.MILLISECONDS);
-                                } catch (InterruptedException ie) {
-                                    return false;
-                                }
-                            }
-                        },
-                        new ThreadFactoryBuilder().setNameFormat("task-update-thread-%d").build());
-
         log.info("OrkesWorkflowExecutor initialized");
     }
 
@@ -140,117 +110,6 @@ public class OrkesWorkflowExecutor extends WorkflowExecutor {
                 orkesExecutionDAOFacade.getWorkflowModel(request.getReRunFromWorkflowId(), true);
         executionDAO.restoreWorkflow(workflowModel);
         return super.rerun(request);
-    }
-
-    @Override
-    boolean scheduleTask(WorkflowModel workflow, List<TaskModel> tasks) {
-        List<TaskModel> tasksToBeQueued;
-        boolean startedSystemTasks = false;
-
-        try {
-            if (tasks == null || tasks.isEmpty()) {
-                return false;
-            }
-
-            // Get the highest seq number
-            int count = workflow.getTasks().stream().mapToInt(TaskModel::getSeq).max().orElse(0);
-
-            for (TaskModel task : tasks) {
-                if (task.getSeq() == 0) { // Set only if the seq was not set
-                    task.setSeq(++count);
-                }
-            }
-
-            // metric to track the distribution of number of tasks within a workflow
-            Monitors.recordNumTasksInWorkflow(
-                    workflow.getTasks().size() + tasks.size(),
-                    workflow.getWorkflowName(),
-                    String.valueOf(workflow.getWorkflowVersion()));
-
-            // Save the tasks in the DAO
-            orkesExecutionDAOFacade.createTasks(tasks);
-
-            List<TaskModel> systemTasks =
-                    tasks.stream()
-                            .filter(task -> systemTaskRegistry.isSystemTask(task.getTaskType()))
-                            .collect(Collectors.toList());
-
-            tasksToBeQueued =
-                    tasks.stream()
-                            .filter(task -> !systemTaskRegistry.isSystemTask(task.getTaskType()))
-                            .collect(Collectors.toList());
-
-            // Traverse through all the system tasks, start the sync tasks, in case of async, queue
-            // the tasks
-            List<Future<TaskModel>> futureExecutions = new ArrayList<>();
-            for (TaskModel task : systemTasks) {
-                WorkflowSystemTask workflowSystemTask = systemTaskRegistry.get(task.getTaskType());
-                if (workflowSystemTask == null) {
-                    throw new NotFoundException(
-                            "No system task found by name %s", task.getTaskType());
-                }
-                if (task.getStatus() != null
-                        && !task.getStatus().isTerminal()
-                        && task.getStartTime() == 0) {
-                    task.setStartTime(System.currentTimeMillis());
-                }
-                if (!workflowSystemTask.isAsync()) {
-                    Future<TaskModel> future =
-                            taskUpdateExecutor.submit(
-                                    () -> {
-                                        workflowSystemTask.start(workflow, task, this);
-                                        return task;
-                                    });
-                    futureExecutions.add(future);
-                    startedSystemTasks = true;
-                } else {
-                    tasksToBeQueued.add(task);
-                }
-            }
-
-            futureExecutions.forEach(
-                    future -> {
-                        try {
-                            TaskModel task = future.get();
-                            orkesExecutionDAOFacade.updateTask(task);
-                        } catch (Exception e) {
-                            throw new NonTransientException(e.getMessage(), e);
-                        }
-                    });
-
-        } catch (Exception e) {
-            List<String> taskIds =
-                    tasks.stream().map(TaskModel::getTaskId).collect(Collectors.toList());
-            String errorMsg =
-                    String.format(
-                            "Error scheduling tasks: %s, for workflow: %s",
-                            taskIds, workflow.getWorkflowId());
-            log.error(errorMsg, e);
-            Monitors.error(OrkesWorkflowExecutor.class.getSimpleName(), "scheduleTask");
-            throw new TerminateWorkflowException(errorMsg);
-        }
-
-        // On addTaskToQueue failures, ignore the exceptions and let WorkflowRepairService take care
-        // of republishing the messages to the queue.
-        try {
-            addTaskToQueue(tasksToBeQueued);
-        } catch (Exception e) {
-            List<String> taskIds =
-                    tasksToBeQueued.stream().map(TaskModel::getTaskId).collect(Collectors.toList());
-            String errorMsg =
-                    String.format(
-                            "Error pushing tasks to the queue: %s, for workflow: %s",
-                            taskIds, workflow.getWorkflowId());
-            log.warn(errorMsg, e);
-            Monitors.error(OrkesWorkflowExecutor.class.getSimpleName(), "scheduleTask");
-        }
-        return startedSystemTasks;
-    }
-
-    private void addTaskToQueue(final List<TaskModel> tasks) {
-        for (TaskModel task : tasks) {
-            addTaskToQueue(task);
-        }
     }
 
     @Override
