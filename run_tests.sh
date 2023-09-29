acbb0b1908e8ec401d465096dfa1e562b96292fc
Viren Baraiya <virenx@gmail.com>
2023-09-29 12:04:02 -0700

    update docker builds

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,18 @@
-./gradlew clean build
-# Create an empty ui build directory... we don't need UI for this build
-mkdir -p docker/tmp/ui/conductor/ui/build
 docker rm -f  conductor_test_container
 docker build -f docker/DockerfileStandalone . -t conductor_test_container
 docker run -dit --name conductor_test_container -p 8899:8080 -p 4535:5000 -t conductor_test_container
+COUNTER=0
+MAX_TIME=120
 while ! curl -s http://localhost:8899/api/metadata/workflow -o /dev/null
 do
-  echo "$(date) - still trying"
+  echo "$(date) - still trying - since $COUNTER second, will wait for $MAX_TIME"
   sleep 1
+  let COUNTER=COUNTER+1
+  if [ $COUNTER -gt $MAX_TIME ];
+  then
+    echo "Exceeded wait time of $MAX_TIME seconds.  Terminating the build"
+    exit 1
+  fi
 done
 sleep 5
 echo "All set - starting tests now"
