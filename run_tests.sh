db760854ec746b581180c68096751ab0f76fd0b8
Viren Baraiya <virenx@gmail.com>
2023-09-28 22:14:31 -0700

    update conductor server version and JDK

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 mkdir -p tmp/ui/conductor/ui/build
 docker rm -f  conductor_test_container
 docker build -f docker/DockerfileStandalone . -t conductor_test_container
-docker run -dit --name conductor_test_container -p 8899:8080 -t conductor_test_container
+docker run -dit --name conductor_test_container -p 8899:8080 -p 4535:5000 -t conductor_test_container
 while ! curl -s http://localhost:8899/api/metadata/workflow -o /dev/null
 do
   echo "$(date) - still trying"
